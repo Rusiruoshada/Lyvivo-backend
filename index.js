f28996823e39ccec0147fede08848fc73d0f3290
roms <rusirusamaraweera749@gmail.com>
2024-06-16 22:22:41 +0530

    create database

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import express from 'express';
-
+// Importing the mongoose package
+import mongoose from 'mongoose';
 
 const app = express();
 const port = 5000;
+const url = '';
+
+const connectDB = async() => {
+    try {
+        const connect = await mongoose.connect( url, {
+            useNewUrlParser: true,
+        });
+        console.log(`MongoDB Connected: ${connect.connection.host}`)
+    }catch (error) {
+        console.error(error.massage);
+
+    }
+}
+
 
 
 app.get('/', (req,res)=> {
