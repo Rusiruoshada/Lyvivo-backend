a510d38ef3010a4ccc8c26bccff0b3a84d4bb11c
roms <rusirusamaraweera749@gmail.com>
2024-06-26 22:35:32 +0530

    found a bug

---
+++ src/models/product.ts
@@ -0,0 +1,36 @@
+import mongoose, { model } from 'mongoose';
+
+
+const ProductSchema = new mongoose.Schema(
+  {
+    productName: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      ref: 'Category',
+      required: true,
+    },
+    weight: {
+      type: String,
+      required: true,
+    },
+    regular_price: {
+      type: Number,
+      required: true,
+    },
+    discount_price: {
+      type: Number,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const productSchema = model('Product', ProductSchema)
