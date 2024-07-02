6273cc3771789ed64208ee8a6c8287fb52553521
roms <rusirusamaraweera749@gmail.com>
2024-07-03 02:14:45 +0530

    create homepage category products

--- index.js
+++ index.js
@@ -31,6 +31,30 @@ app.get('/', (req,res)=> {
     res.send('backend working')
 })
 
+app.get('/api/homepageProduct', async(req, res) => {
+    const category = {show: 'Grocery', show1: 'Best Savings', show2: 'Electronics'}
+    try{
+        const grocery = await Product.find({category:category.show});
+        if (!grocery) {
+            return res.status(500).json({ message: 'Server unavailable now!' });
+        }
+        const bestSavings = await Product.find({category:category.show1});
+        if (!bestSavings) {
+            return res.status(500).json({ message: 'Server unavailable now!' });
+        }
+        const Electronics = await Product.find({category:category.show2});
+        if (!Electronics) {
+            return res.status(500).json({ message: 'Server unavailable now!' });
+        }
+
+
+        res.json(grocery);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
 
 app.get('/api/allProduct',  async(req,res) => {
     try{
